f25594874b1eb5b51154278f22534dd0d9e28439
Alex Aguirre <teamaguirre@gmail.com>
2023-01-12 00:13:31 -0800

    added the frontend index and app code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import {Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import Workouts from './pages/WorkoutLog';
+import Error404 from './pages/Error404';
+import Navbar from './components/Navbar';
+import CurrentUserProvider from './context/CurrentUser';
+import MealLog from './pages/MealLog';
+import UserDataForm from './components/UserDataForm';
+
 function App() {
   return (
-    <div className="App">
-     <h1>FitTrack App V2</h1>
-    </div>
+    <CurrentUserProvider>
+      <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/login' element={<Login/>}/>
+          <Route exact path='/signup' element={<Signup/>}/>
+          <Route exact path='/profile' element={<Profile/>}/>
+          <Route exact path='/meals' element={<MealLog/>}/>
+          <Route exact path='/workouts' element={<Workouts/>}/>
+          <Route exact path='/userdata' element={<UserDataForm/>}/>
+          
+          <Route path='/*' element={<Error404 />} />
+        </Routes>
+    </CurrentUserProvider>
   );
 }
 
