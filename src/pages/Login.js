ac164d29f5c06fc5b9393de465a247140eea6d4b
Alex Aguirre <teamaguirre@gmail.com>
2023-01-13 19:18:41 -0800

    testing the heroku path

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -30,7 +30,7 @@ function Login() {
     async function handleSubmit(e) {
         e.preventDefault();
                 // send a request to the server to verify the email and password
-        const response = await fetch(`${process.env.HEROKU_HOST_URI}/authentication`, { // to be changed to server route.
+        const response = await fetch(`${process.env.HEROKU_HOST_URI}authentication`, { // to be changed to server route.
             method: 'POST',
             credentials: 'include',
             headers: {
