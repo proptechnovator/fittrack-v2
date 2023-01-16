aa2e88efce70a71fe21b08daf23955c833d9b830
Alex Aguirre <teamaguirre@gmail.com>
2023-01-16 14:55:48 -0800

    Deploying backend

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const defineCurrentUser = require('./middleware/defineCurrentUser');
 
 //middleware
 var corsOptions ={
-    origin: `http://localhost:5501` ,
+    origin: `https://fittrack-frontend.herokuapp.com/` ,
     methods:"GET,PUT,POST,DELETE,PATCH",
     changeOrigin:true,
     credentials: true
