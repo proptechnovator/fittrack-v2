ac2ec95ef522a44a94f5e4882337d596548a5aa0
Alex Aguirre <teamaguirre@gmail.com>
2023-01-17 02:21:51 -0800

    icon navbar images  responsiveness

--- src/components/IconUserNav.js
+++ src/components/IconUserNav.js
@@ -7,26 +7,26 @@ const IconUserNav = () => {
   return (
         <section className='iconNav'>
           <div>  
-               <a className='w-25 link' href={ !currentUser ? '/login' : '/workouts'}>
-               <img className='w-100' alt='dumbbell' src='./fitness-icon-blue.png'/>
+               <a className='link' href={ !currentUser ? '/login' : '/workouts'}>
+               <img alt='dumbbell' src='./fitness-icon-blue.png'/>
                </a>
                <h2 className='fw-bold mt-3 w-50'>Workout Log</h2>
           </div>
           <div>     
-               <a className='w-25 link' href={ !currentUser ? '/login' : '/meals'}>
-               <img className='w-100' alt='healthy-food' src='./healthy-food-icon-orange.png'/>
+               <a className='link' href={ !currentUser ? '/login' : '/meals'}>
+               <img  alt='healthy-food' src='./healthy-food-icon-orange.png'/>
                </a>
                <h2 className='fw-bold mt-3 w-50'>Meal Log</h2>
          </div>    
          <div>
-            <a className='w-25 link' href={ !currentUser ? '/login' : '/' /* to be changed later */}>
-            <img className='w-100'alt='calculator-icon' src='./calculate-math-icon-blue.png'/>
+            <a className='link' href={ !currentUser ? '/login' : '/' /* to be changed later */}>
+            <img alt='calculator-icon' src='./calculate-math-icon-blue.png'/>
             </a>
             <h2 className='fw-bold mt-3 w-50'>Calculators</h2>
          </div>
          <div>   
-            <a className='w-25 link' href={ !currentUser ? '/login' : '/profile'}>
-            <img className='w-100' alt='personal-growth-icon' src='./personal-growth-icon.png'/>
+            <a className='link' href={ !currentUser ? '/login' : '/profile'}>
+            <img alt='personal-growth-icon' src='./personal-growth-icon.png'/>
             </a>
             <h2 className='fw-bold mt-3 w-50'>Progress</h2>
         </div>   
