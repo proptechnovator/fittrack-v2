395e50621a68d98754b687209ed9cd33dafbd96a
Alex Aguirre <teamaguirre@gmail.com>
2023-01-17 00:44:58 -0800

    styled the greeting

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -2,7 +2,7 @@ function Error404() {
     return (
         <div className='load' style={{textAlign:'center', marginTop: '25px'}}>
             <h1>404: PAGE NOT FOUND</h1>
-            <img className='my-1' src="../gains-loading.jpg" alt="gains loading " />
+            <img className='my-1' src="../gains-loading-orange.jpg" alt="gains loading " />
             <h4 id='greet' className='my-3'>Oops, having trouble lifting this page onto the app!</h4>
             <a href="./" ><button className="btn btn-secondary fw-bold">Let's Get Back to Tracking</button></a>
         </div>
